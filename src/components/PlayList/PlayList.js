28b69be1d39335a522d635020a6452865d6374c8
easoftwareGit <easoftware@gmail.com>
2023-04-26 09:07:31 -0700

    Got list header components basic code

--- src/components/PlayList/PlayList.js
+++ src/components/PlayList/PlayList.js
@@ -2,19 +2,16 @@ import React from "react";
 
 import './PlayList.css';
 import './PlayListHeader';
-import TrackList from "../TrackList/TrackList";
+// import TrackList from "../TrackList/TrackList";
+import TrackListContainer from "../TrackList/TrackListContainer";
 import PlayListHeader from "./PlayListHeader";
 
 export default function PlayList() {
   return (
     <div className="AppList PlayList">
-      <PlayListHeader />
-      {/* <input 
-        type='text'
-        placeholder="Playlist Name..."
-      >
-      </input> */}
-      <TrackList />
+      <TrackListContainer headerObj={<PlayListHeader />}/>
+      {/* <PlayListHeader />
+      <TrackList /> */}
     </div>
   );
 }
